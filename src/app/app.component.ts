b31330fd9f708c9afd772e5aa4bbdd5834637fbb
bassel <bassel.islam@gmail.com>
2026-04-14 15:55:19 +0200

    created and finished investmnet input component .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,22 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
   calculateInvestmentResults(data: InvestmentInput) {
     const annualData = [];
     const { initialInvestment, annualInvestment, expectedReturn, duration } =
@@ -31,6 +40,6 @@ export class AppComponent {
       });
     }
 
-    console.log(annualData);
+    this.resultsData = annualData;
   }
 }
